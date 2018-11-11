9436b1a4e0d951e385071b29cdb22e8dca484fc8
AnderBalles98 <44950106+AnderBalles98@users.noreply.github.com>
2018-11-11 14:03:58 -0500

    Update 568.java

--- 568.java
+++ 568.java
@@ -1,39 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
 import java.math.BigInteger;
+import java.util.Locale;
+
 
 /**
  * @author ANDERSON BALLESTEROS
  */
- 
+
 class Main {
 
-    public static void main(String[] args) {
-        //System.out.println("");
-        BigInteger aa=new BigInteger("6227020800"); 
-        BigInteger ab=new BigInteger("10000");
-        Scanner leer = new Scanner(System.in);
-        while (leer.hasNext()) {
-                int a = leer.nextInt();
-                BigInteger respuesta = new BigInteger("1");
-                if (a<0) {                    
-                for (int i = 0; i > a; i--) {
-                    respuesta = respuesta.multiply(BigInteger.valueOf(i - 1));
-                }
-                }if (a>0) {
-                for (int i = 0; i < a; i++) {
-                    respuesta = respuesta.multiply(BigInteger.valueOf(i + 1));
-                }    
-                }
-                if (respuesta.compareTo(aa)>0) {
-                    System.out.println("Overflow!"+" "+a);
-                }if (respuesta.compareTo(ab)<0) {
-                    System.out.println("Underflow! "+a);
-                }if (respuesta.compareTo(aa)<0&&respuesta.compareTo(ab)>0) {
-                 System.out.println(respuesta+" "+a);   
-                }
+    public static void main(String[] args) throws IOException {
+        BufferedReader leer = new BufferedReader(new InputStreamReader(System.in));
+        while (leer.ready()) {
+            Integer num = Integer.parseInt(leer.readLine());
+            BigInteger sol = new BigInteger("1");
+            for (int i = 1; i <= num; i++) {
+                sol = sol.multiply(BigInteger.valueOf(i));
+            }
+            String text = sol + "";
+            for (int i = text.length() - 1; i >= 0; i--) {
+                if (text.charAt(i) != '0') {
+                    System.out.format(Locale.ROOT, "%5d -> %c\n", num, text.charAt(i));
+                    break;
                 }
             }
+        }
 
-        
     }
-
+}
