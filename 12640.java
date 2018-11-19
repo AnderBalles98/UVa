fd3feff9ac7f922026507542259f3cac32047cc6
AnderBalles98 <44950106+AnderBalles98@users.noreply.github.com>
2018-11-19 11:26:22 -0500

    Create 12640.java

---
+++ 12640.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+/**
+ * @author ANDERSON BALLESTEROS
+ */
+
+class Main {
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader leer = new BufferedReader(new InputStreamReader(System.in));
+        while (true) {
+            String line[] = leer.readLine().split(" ");
+            int max = 0, acum = 0, num;
+            for (int i = 0; i < line.length; i++) {
+                num = Integer.parseInt(line[i]);
+                acum+=num;
+                max = Integer.max(max, acum);
+                if (acum<0) {
+                    acum = 0;
+                }
+            }
+            System.out.println(max);
+        }
+    }
+}
