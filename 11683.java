e6d7d29ea46c5199fa939b947e356e5178c9e9f9
AnderBalles98 <44950106+AnderBalles98@users.noreply.github.com>
2019-09-01 23:43:10 -0500

    Create 11683.java

---
+++ 11683.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+/**
+ * @author ANDERSON BALLESTEROSS
+ */
+
+class Main {
+
+    public static void main(String[] args) throws IOException {
+     
+        Scanner read = new Scanner(System.in);
+        
+        int height, lenght;
+        while ((height=read.nextInt())!=0) {
+            lenght=read.nextInt();
+            int beforeHeight = read.nextInt();
+            int cuts = height - beforeHeight;
+            for (int i = 1; i < lenght; i++) {
+                int actuallyHeight = read.nextInt();
+                if (actuallyHeight<beforeHeight) {
+                    cuts+=beforeHeight-actuallyHeight;
+                }
+                beforeHeight=actuallyHeight;
+            }
+            System.out.println(cuts);
+        }
+        
+        
+    }
+
+}
